5cf017eb986b15606f164666156b7145341be530
chelseylin <linqiaoshu1992@gmail.com>
2017-10-30 05:31:27 -0400

    Added footer. Devided body sections for index page.

---
+++ footer.php
@@ -0,0 +1,91 @@
+<?php /*wp_footer();*/ ?>	
+
+	<footer>
+		<div class="footer-container">
+			<div class="footer-widgets-wrap clearfix" style="z-index: 2">
+				<div class="row clearfix">
+					<div class="col-md-5">
+						<div class="widget clearfix">
+							<div class="row clearfix">
+								<div class="col-md-8 clearfix">
+									<p class="footer-title">Ainushi Translations</p>
+									<p class="footer-description">Where Asian Romance and Actions Come to Life</p>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-7">
+						<div class="row clearfix">
+							<div class="col-md-4">
+								<div class="widget widget_links app_landing_widget_link clearfix">
+									<h4>In News</h4>
+									<ul>
+										<li><a href="http://codex.wordpress.org/">Documentation</a></li>
+										<li><a href="http://wordpress.org/support/forum/requests-and-feedback">Feedback</a></li>
+										<li><a href="http://wordpress.org/extend/plugins/">Plugins</a></li>
+										<li><a href="http://wordpress.org/support/">Support Forums</a></li>
+										<li><a href="http://wordpress.org/extend/themes/">Themes</a></li>
+									</ul>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="widget widget_links app_landing_widget_link clearfix">
+									<h4>About Us</h4>
+
+									<ul>
+										<li><a href="http://codex.wordpress.org/">Documentation</a></li>
+										<li><a href="http://wordpress.org/support/forum/requests-and-feedback">Feedback</a></li>
+										<li><a href="http://wordpress.org/extend/plugins/">Plugins</a></li>
+										<li><a href="http://wordpress.org/support/">Support Forums</a></li>
+										<li><a href="http://wordpress.org/extend/themes/">Themes</a></li>
+									</ul>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="widget widget_links app_landing_widget_link clearfix">
+									<h4>Support</h4>
+
+									<ul>
+										<li><a href="http://codex.wordpress.org/">Documentation</a></li>
+										<li><a href="http://wordpress.org/support/forum/requests-and-feedback">Feedback</a></li>
+										<li><a href="http://wordpress.org/extend/plugins/">Plugins</a></li>
+										<li><a href="http://wordpress.org/support/">Support Forums</a></li>
+										<li><a href="http://wordpress.org/extend/themes/">Themes</a></li>
+									</ul>
+								</div>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Copyrights
+	============================================= -->
+	<div id="copyrights">
+		<div class="container clearfix">
+			<div class="col_half">
+				<p class="copyright">Copyrights &copy; <?php echo date('Y'); ?> All Rights Reserved by Ainushi Translations.</p>
+			</div>
+		</div>
+
+	</div><!-- #copyrights end -->
+
+		<div class="video-wrap" style="height: 100%;position: absolute; left: 0; top: 0; z-index: 1;">
+			<div class="video-overlay" style="background: -moz-linear-gradient(top,  rgba(30,35,42,1) 21%,, rgba(30,35,42,0) 100%); background: -webkit-linear-gradient(top,  rgba(30,35,42,1) 21%,rgba(30,35,42,0) 100%); background: linear-gradient(to bottom,  rgba(30,35,42,1) 21%,rgba(30,35,42,0) 100%); filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#001e232a', endColorstr='#1e232a',GradientType=0 );">
+			</div>
+		</div>
+
+	</footer><!-- #footer end -->
+
+
+<!-- Javascripts
+============================================= -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+<script src="<?php echo bloginfo('template_url'); ?>/js/bootstrap.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php get_header(); ?>
 
 	<div class="main-container">
-		
+		<div class="banner">
+		</div>
+		<div class="announcement">
+		</div>
+		<div class="releases">
+		</div>
 	</div>
 
-<!-- Javascripts
-============================================= -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
-<script src="<?php echo bloginfo('template_url'); ?>/js/bootstrap.js"></script>
-</body>
-</html>
+<?php get_footer(); ?>
