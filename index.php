c9848c62ab137185fbe125b3c3449742fbcc2aff
chelseylin <linqiaoshu1992@gmail.com>
2017-10-30 02:34:50 -0400

    Add index.php page and imported and tested bootstrap.

---
+++ index.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<!-- Metas
+	============================================= -->
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Stylesheets
+	============================================= -->
+	<link rel="stylesheet" type="text/css" href="<?php echo bloginfo('template_url'); ?>/css/bootstrap.css"
+	<link rel="stylesheet" type="text/css" href="<?php echo bloginfo('template_url'); ?>/style.css" />
+
+	<!-- Document Title
+	============================================= -->
+	<title><?php echo bloginfo('name'); ?></title>
+
+</head>
+
+<body>
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+		<a class="navbar-brand" href="<?php echo get_home_url(); ?>">Ainushi Translations</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    		<span class="navbar-toggler-icon"></span>
+  		</button>
+  		<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    		<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      			<li class="nav-item active">
+        			<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+     			 </li>
+      			<li class="nav-item">
+       				<a class="nav-link" href="#">Link</a>
+      			</li>
+      			<li class="nav-item">
+        			<a class="nav-link" href="#">Disabled</a>
+      			</li>
+    		</ul>
+  		</div>
+	</nav>
+
+<!-- Javascripts
+============================================= -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+<script src="<?php echo bloginfo('template_url'); ?>/js/bootstrap.js"></script>
+</body>
+</html>
