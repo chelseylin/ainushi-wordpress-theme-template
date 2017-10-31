650f2904bde71b64a7dd946a3346b5dc67ceae8b
chelseylin <linqiaoshu1992@gmail.com>
2017-10-31 06:24:58 -0400

    finished mockup for index.php, possible slider in future

--- index.php
+++ index.php
@@ -14,14 +14,14 @@
 			<div class="announcement-container">
 				<h4>~ Announcement ~</h4>
 				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, conse </p>
-				<a href="#">Read More</a>
+				<a href="#" class="no-hover">Read More</a>
 			</div>
 		</div>
 		<div class="releases">
 			<div class="releases-container">
 				<h4>Latest Releases</h4>
 				<p class="releases-description">-- checkout our newest updates --</p>
-				<div row>
+				<div class="release-card row">
 				    <div class="card horizontal">
 				      	<div class="card-image">
 				       		<img src="<?php echo bloginfo('template_url');?>/TA.jpg"">
@@ -29,19 +29,123 @@
 				      	<div class="card-stacked">
 				        	<div class="card-content">
 				        		<div class="row">
-					        		<div class="col-12 col-lg-8">
-					        			<h2 class="header">ABC Chapter 3 - Iweonw WIon</h2>
-					        		</div>
+				        			<div class="col-12 col-lg-8">
+				          				<h2 class="header"><a href="#" class="no-hover">Mob: Chapter 22 – Please no prohibition on going out! Hey, someone listen to me!?</a></h2>
+				        			</div>
 					        		<div class="col-lg-4">
 					        			<p class="date">31 October, 2017</p>
 					        		</div>
 					        	</div>
 					        	<div class="summary">
-				          			<p>I am a very simple card. I am good at containing small bits of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information. ts of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information I am a very simple card. I am good at containing small bits of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information. ts of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information</p>
+				          			<p>I am a very simple card. I am good at containing small bits of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information. ts of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information I am a very simple card. I am good at containing small bits of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information. ts of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information d. I am good at containing small bits of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bit... </p>
 				        		</div>
 				        	</div>
-				        	<div class="card-action">
-				          		<a href="#">This is a link</a>
+				        	<div class="card-action readmore">
+				        		<a href="#" class="no-hover">Read More</a>
+				        	</div>
+				      	</div>
+					</div>
+				</div>
+
+				<div class="release-card row">
+				    <div class="card horizontal">
+				      	<div class="card-image">
+				       		<img src="<?php echo bloginfo('template_url');?>/TA.jpg"">
+				      	</div>
+				      	<div class="card-stacked">
+				        	<div class="card-content">
+				        		<div class="row">
+				        			<div class="col-12 col-lg-8">
+				          				<h2 class="header"><a href="#" class="no-hover">Mob: Chapter 22 – Please no prohibition on going out! Hey, someone listen to me!?</a></h2>
+				        			</div>
+					        		<div class="col-lg-4">
+					        			<p class="date">31 October, 2017</p>
+					        		</div>
+					        	</div>
+					        	<div class="summary">
+				          			<p>I am a very simple card. I am good at containing small bits of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information. ts of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information I am a very simple card. I am good at containing small bits of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information. ts of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information d. I am good at containing small bits of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bit... </p>
+				        		</div>
+				        	</div>
+				        	<div class="card-action readmore">
+				        		<a href="#" class="no-hover">Read More</a>
+				        	</div>
+				      	</div>
+					</div>
+				</div>
+
+				<div class="release-card row">
+				    <div class="card horizontal">
+				      	<div class="card-image">
+				       		<img src="<?php echo bloginfo('template_url');?>/TA.jpg"">
+				      	</div>
+				      	<div class="card-stacked">
+				        	<div class="card-content">
+				        		<div class="row">
+				        			<div class="col-12 col-lg-8">
+				          				<h2 class="header"><a href="#" class="no-hover">Mob: Chapter 22 – Please no prohibition on going out! Hey, someone listen to me!?</a></h2>
+				        			</div>
+					        		<div class="col-lg-4">
+					        			<p class="date">31 October, 2017</p>
+					        		</div>
+					        	</div>
+					        	<div class="summary">
+				          			<p>I am a very simple card. I am good at containing small bits of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information. ts of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information I am a very simple card. I am good at containing small bits of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information. ts of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information d. I am good at containing small bits of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bit... </p>
+				        		</div>
+				        	</div>
+				        	<div class="card-action readmore">
+				        		<a href="#" class="no-hover">Read More</a>
+				        	</div>
+				      	</div>
+					</div>
+				</div>
+
+				<div class="release-card row">
+				    <div class="card horizontal">
+				      	<div class="card-image">
+				       		<img src="<?php echo bloginfo('template_url');?>/TA.jpg"">
+				      	</div>
+				      	<div class="card-stacked">
+				        	<div class="card-content">
+				        		<div class="row">
+				        			<div class="col-12 col-lg-8">
+				          				<h2 class="header"><a href="#" class="no-hover">Mob: Chapter 22 – Please no prohibition on going out! Hey, someone listen to me!?</a></h2>
+				        			</div>
+					        		<div class="col-lg-4">
+					        			<p class="date">31 October, 2017</p>
+					        		</div>
+					        	</div>
+					        	<div class="summary">
+				          			<p>I am a very simple card. I am good at containing small bits of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information. ts of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information I am a very simple card. I am good at containing small bits of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information. ts of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information d. I am good at containing small bits of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bit... </p>
+				        		</div>
+				        	</div>
+				        	<div class="card-action readmore">
+				        		<a href="#" class="no-hover">Read More</a>
+				        	</div>
+				      	</div>
+					</div>
+				</div>
+
+				<div class="release-card row">
+				    <div class="card horizontal">
+				      	<div class="card-image">
+				       		<img src="<?php echo bloginfo('template_url');?>/TA.jpg"">
+				      	</div>
+				      	<div class="card-stacked">
+				        	<div class="card-content">
+				        		<div class="row">
+				        			<div class="col-12 col-lg-8">
+				          				<h2 class="header"><a href="#" class="no-hover">Mob: Chapter 22 – Please no prohibition on going out! Hey, someone listen to me!?</a></h2>
+				        			</div>
+					        		<div class="col-lg-4">
+					        			<p class="date">31 October, 2017</p>
+					        		</div>
+					        	</div>
+					        	<div class="summary">
+				          			<p>I am a very simple card. I am good at containing small bits of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information. ts of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information I am a very simple card. I am good at containing small bits of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information. ts of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bits of information d. I am good at containing small bits of information. I am a very simple card. I am good at coam gooormation. I am a very simple card. I am good at containing small bit... </p>
+				        		</div>
+				        	</div>
+				        	<div class="card-action readmore">
+				        		<a href="#" class="no-hover">Read More</a>
 				        	</div>
 				      	</div>
 					</div>
